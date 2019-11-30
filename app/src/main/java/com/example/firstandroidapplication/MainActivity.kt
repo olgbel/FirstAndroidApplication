61d1fd7cde55f8f8b4c317814728ae03f4e3d7a7
OlgaKC <olgbelous@gmail.com>
2019-11-30 14:32:30 +0300

    first task

--- app/src/main/java/com/example/firstandroidapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstandroidapplication/MainActivity.kt
@@ -1,114 +1,56 @@
 package com.example.firstandroidapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.firstandroidapplication.dto.*
 import com.example.firstandroidapplication.adapter.PostAdapter
+import com.example.firstandroidapplication.client.Api
+import com.example.firstandroidapplication.dto.Post
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.GsonSerializer
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.util.KtorExperimentalAPI
+import kotlinx.coroutines.*
+import kotlin.coroutines.EmptyCoroutineContext
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var posts = mutableListOf(
-            Post(
-                4,
-                PostType.POST,
-                "Netology",
-                "Kotlin MeetUp!",
-                1566818240,
-                Comments(1),
-                Likes(1, true),
-                Reposts(5),
-                null
-            ),
-            Post(
-                3,
-                PostType.EVENT,
-                "Netology",
-                "See where you are!",
-                1566418240,
-                Comments(5),
-                Likes(5, true),
-                Reposts(0),
-                Geo(1, -90.9, 90.0, "SPb VO 7 64")
-            ),
-            Post(
-                2,
-                PostType.MEDIA,
-                "Netology",
-                "Watch our video!",
-                1566518240,
-                Comments(0),
-                Likes(3, false),
-                Reposts(1),
-                null,
-                "https://www.youtube.com/watch?v=WhWc3b3KhnY"
-            ),
-            Post(
-                9,
-                PostType.ADVERTISING,
-                "Netology",
-                "Test advertising delete",
-                9090,
-                Comments(9),
-                Likes(0, false),
-                Reposts(2),
-                null,
-                null,
-                "https://google.com/"
-            ),
-            Post(
-                6,
-                PostType.POST,
-                "Netology",
-                "Our network is growing!",
-                786756,
-                Comments(5),
-                Likes(5, true),
-                Reposts(0),
-                null
-            ),
-            Post(
-                1,
-                PostType.POST,
-                "Netology",
-                "First it in our network!",
-                9955334,
-                Comments(9),
-                Likes(0, false),
-                Reposts(2),
-                null
-            ),
-            Post(
-                7,
-                PostType.ADVERTISING,
-                "Netology",
-                "Go through the link: ",
-                9090,
-                Comments(9),
-                Likes(0, false),
-                Reposts(2),
-                null,
-                null,
-                "https://netology.ru/"
-            )
-        )
+        fetchData()
 
+
+    }
+
+    private fun fetchData() = launch {
+        // UI - Progress Bar
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
 
+        val posts = withContext(Dispatchers.IO) {
+            Api.client.get<MutableList<Post>>(Api.url)
+        }
         with(recyclerView){
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = PostAdapter(posts)
 
         }
-
-
+        // Hide ProgressBar
+//        Toast.makeText(this@MainActivity, "length of list ${posts.size}", Toast.LENGTH_LONG).show()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancel()
+    }
+
+
+
 
 
 }
---
+++ app/src/main/java/com/example/firstandroidapplication/client/Api.kt
@@ -0,0 +1,20 @@
+package com.example.firstandroidapplication.client
+
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.GsonSerializer
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.http.ContentType
+
+object Api {
+    const val url = "https://raw.githubusercontent.com/olgbel/firstAndroidApplication/master/posts.json"
+
+    val client = HttpClient {
+        install(JsonFeature) {
+            acceptContentTypes = listOf(
+                ContentType.Text.Plain,
+                ContentType.Application.Json
+            )
+            serializer = GsonSerializer()
+        }
+    }
+}
